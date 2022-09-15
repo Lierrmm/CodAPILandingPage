f37e334127ea4a7069a034bcbf2572cd7f6e2506
Lierrmm <lierrmm@gmail.com>
2022-09-15 17:44:46 +0100

    Added game support widget

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import CodeSupport from "./templates/CodeSupport.svelte";
   import Footer from "./templates/Footer.svelte";
+  import GameSupport from "./templates/GameSupport.svelte";
   import Header from "./templates/Header.svelte";
   import Hero from "./templates/Hero.svelte";
 </script>
@@ -10,6 +11,7 @@
 <main>
     <Hero />
     <CodeSupport />
+    <GameSupport />
 </main>
 
 <Footer />
---
+++ src/templates/GameSupport.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+// Vanguard
+// ModernWarfare
+// Warzone
+// Cold War
+
+// MW2 🔜
+    import ColdWar from '../assets/coldwar.webp';
+    import Vanguard from '../assets/vanguard.webp';
+    import MW from '../assets/mw.jpg';
+    // import MW2 from '../assets/mw2.jpg';
+    import WZ from '../assets/wz.jpg';
+
+    interface Game {
+        title: string,
+        image: string
+    }
+
+    let games: Game[] = [
+        // {
+        //     "title": "Modern Warfare 2",
+        //     "image": MW2
+        // },
+        {
+            "title": "Vanguard",
+            "image": Vanguard
+        },
+        {
+            "title": "Cold War",
+            "image": ColdWar
+        },
+        {
+            "title": "Modern Warfare",
+            "image": MW
+        },
+        {
+            "title": "Warzone",
+            "image": WZ
+        }
+    ]
+
+</script>
+
+<section id="game" class="text-gray-300 body-font">
+    <div class="container px-5 py-12 mx-auto mb-16">
+      <div class="flex flex-col text-center w-full mb-16">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-rose-400">Supported Games</h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Support for the latest Call of Duty titles.</p>
+      </div>
+      <div class="flex flex-wrap -m-4 justify-center text-center">
+        {#each games as game}
+        <div class="card card-compact w-40 h-52 bg-base-100 shadow-xl ml-4" title="{game.title}">
+            <figure><img src="{game.image}" alt="{game.title}" /></figure>
+        </div>
+        {/each}
+      </div>
+    </div>
+</section>
