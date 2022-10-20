5a696a60981a24d786038882101a503701cf933f
Lierrmm <lierrmm@gmail.com>
2022-10-20 18:57:48 +0100

    feat: add link to nuget package

--- src/templates/CodeSupport.svelte
+++ src/templates/CodeSupport.svelte
@@ -1,38 +1,67 @@
 <section id="lang" class="text-gray-300 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-rose-400">Supported Languages</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">We offer support for multiple languages so you aren't restricted to a single runtime.</p>
-      </div>
-      <div class="flex flex-wrap -m-4 justify-center text-center">
-        <a href="https://docs.codapi.dev" target="_blank" rel="noreferrer noopener" class="p-4 md:w-1/4 sm:w-1/2 w-full">
-          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-            <i class="devicon-typescript-plain colored text-6xl"></i>
-            <h2 class="title-font font-medium text-3xl text-rose-400">Typscript</h2>
-            <p class="leading-relaxed">Fully Supported</p>
-          </div>
-        </a>
-        <a href="https://github.com/TodoLodo/cod-python-api" target="_blank" rel="noreferrer noopener" class="p-4 md:w-1/4 sm:w-1/2 w-full">
-          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-            <i class="devicon-python-plain colored text-6xl"></i>
-            <h2 class="title-font font-medium text-3xl text-rose-400">Python</h2>
-            <p class="leading-relaxed">Fully Supported</p>
-          </div>
-        </a>
-        <a href="https://pub.dev/packages/cod_api" target="_blank" rel="noreferrer noopener" class="p-4 md:w-1/4 sm:w-1/2 w-full">
-          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-            <i class="devicon-flutter-plain colored text-6xl"></i>
-            <h2 class="title-font font-medium text-3xl text-rose-400">Flutter</h2>
-            <p class="leading-relaxed">Fully Supported</p>
-          </div>
-        </a>
-        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-            <i class="devicon-csharp-plain text-purple-400 text-6xl"></i>
-            <h2 class="title-font font-medium text-3xl text-rose-400">C#</h2>
-            <p class="leading-relaxed">Coming Soon™️</p>
-          </div>
-        </div>
-      </div>
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1
+        class="sm:text-3xl text-2xl font-medium title-font mb-4 text-rose-400"
+      >
+        Supported Languages
+      </h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+        We offer support for multiple languages so you aren't restricted to a
+        single runtime.
+      </p>
     </div>
+    <div class="flex flex-wrap -m-4 justify-center text-center">
+      <a
+        href="https://docs.codapi.dev"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="p-4 md:w-1/4 sm:w-1/2 w-full"
+      >
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <i class="devicon-typescript-plain colored text-6xl" />
+          <h2 class="title-font font-medium text-3xl text-rose-400">
+            Typscript
+          </h2>
+          <p class="leading-relaxed">Fully Supported</p>
+        </div>
+      </a>
+      <a
+        href="https://github.com/TodoLodo/cod-python-api"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="p-4 md:w-1/4 sm:w-1/2 w-full"
+      >
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <i class="devicon-python-plain colored text-6xl" />
+          <h2 class="title-font font-medium text-3xl text-rose-400">Python</h2>
+          <p class="leading-relaxed">Fully Supported</p>
+        </div>
+      </a>
+      <a
+        href="https://pub.dev/packages/cod_api"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="p-4 md:w-1/4 sm:w-1/2 w-full"
+      >
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <i class="devicon-flutter-plain colored text-6xl" />
+          <h2 class="title-font font-medium text-3xl text-rose-400">Flutter</h2>
+          <p class="leading-relaxed">Fully Supported</p>
+        </div>
+      </a>
+      <a
+        href="https://www.nuget.org/packages/CSharp-CODAPI"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="p-4 md:w-1/4 sm:w-1/2 w-full"
+      >
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <i class="devicon-csharp-plain text-purple-400 text-6xl" />
+          <h2 class="title-font font-medium text-3xl text-rose-400">C#</h2>
+          <p class="leading-relaxed">Fully Supported</p>
+        </div>
+      </a>
+    </div>
+  </div>
 </section>
