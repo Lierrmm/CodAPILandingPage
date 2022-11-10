9706d5c7662600bb5c16b03758977fd74a5ab2d4
Liam <lgaskell@freewebstore.com>
2022-11-10 18:53:32 +0000

    feat: clean up supported languages

--- src/templates/CodeSupport.svelte
+++ src/templates/CodeSupport.svelte
@@ -1,3 +1,44 @@
+<script lang="ts">
+  interface Languages {
+    title: string;
+    icon: string;
+    supported: boolean;
+    link: string;
+    css: string;
+  }
+
+  let languages: Languages[] = [
+    {
+      title: "Typescript",
+      icon: "devicon-typescript-plain",
+      supported: true,
+      link: "https://docs.codapi.dev",
+      css: "colored",
+    },
+    {
+      title: "Python",
+      icon: "devicon-python-plain",
+      supported: true,
+      link: "https://github.com/TodoLodo/cod-python-api",
+      css: "colored",
+    },
+    {
+      title: "Flutter",
+      icon: "devicon-flutter-plain",
+      supported: true,
+      link: "https://pub.dev/packages/cod_api",
+      css: "colored",
+    },
+    {
+      title: "C#",
+      icon: "devicon-csharp-plain",
+      supported: true,
+      link: "https://www.nuget.org/packages/CSharp-CODAPI",
+      css: "text-purple-400",
+    },
+  ];
+</script>
+
 <section id="lang" class="text-gray-300 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
@@ -12,56 +53,24 @@
       </p>
     </div>
     <div class="flex flex-wrap -m-4 justify-center text-center">
-      <a
-        href="https://docs.codapi.dev"
-        target="_blank"
-        rel="noreferrer noopener"
-        class="p-4 md:w-1/4 sm:w-1/2 w-full"
-      >
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <i class="devicon-typescript-plain colored text-6xl" />
-          <h2 class="title-font font-medium text-3xl text-rose-400">
-            Typscript
-          </h2>
-          <p class="leading-relaxed">Fully Supported</p>
-        </div>
-      </a>
-      <a
-        href="https://github.com/TodoLodo/cod-python-api"
-        target="_blank"
-        rel="noreferrer noopener"
-        class="p-4 md:w-1/4 sm:w-1/2 w-full"
-      >
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <i class="devicon-python-plain colored text-6xl" />
-          <h2 class="title-font font-medium text-3xl text-rose-400">Python</h2>
-          <p class="leading-relaxed">Fully Supported</p>
-        </div>
-      </a>
-      <a
-        href="https://pub.dev/packages/cod_api"
-        target="_blank"
-        rel="noreferrer noopener"
-        class="p-4 md:w-1/4 sm:w-1/2 w-full"
-      >
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <i class="devicon-flutter-plain colored text-6xl" />
-          <h2 class="title-font font-medium text-3xl text-rose-400">Flutter</h2>
-          <p class="leading-relaxed">Fully Supported</p>
-        </div>
-      </a>
-      <a
-        href="https://www.nuget.org/packages/CSharp-CODAPI"
-        target="_blank"
-        rel="noreferrer noopener"
-        class="p-4 md:w-1/4 sm:w-1/2 w-full"
-      >
-        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <i class="devicon-csharp-plain text-purple-400 text-6xl" />
-          <h2 class="title-font font-medium text-3xl text-rose-400">C#</h2>
-          <p class="leading-relaxed">Fully Supported</p>
-        </div>
-      </a>
+      {#each languages as lang}
+        <a
+          href={lang.link}
+          target="_blank"
+          rel="noreferrer noopener"
+          class="p-4 md:w-1/4 sm:w-1/2 w-full"
+        >
+          <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+            <i class="{lang.icon} {lang.css} text-6xl" />
+            <h2 class="title-font font-medium text-3xl text-rose-400">
+              {lang.title}
+            </h2>
+            <p class="leading-relaxed">
+              {lang.supported ? "Fully Supported" : "Coming Soon"}
+            </p>
+          </div>
+        </a>
+      {/each}
     </div>
   </div>
 </section>
